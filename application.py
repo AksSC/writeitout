b0d75806fe2aca4553f2b3e43ddc5b83664fdb6b
AksSC <kanodiaaks@gmail.com>
2021-05-27 08:02:43 +0000

    changed requirements

--- application.py
+++ application.py
@@ -3,7 +3,6 @@ import os
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import login_required
