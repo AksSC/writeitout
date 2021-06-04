6d2550b4a86cd3d68a852c11d5ea5572e8ed6eae
AksSC <62467654+AksSC@users.noreply.github.com>
2021-06-04 16:06:39 +0530

    Added PostgreSQL

--- application.py
+++ application.py
@@ -24,7 +24,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///writeitout.db")
+db = SQL(os.getenv("DATTABASE_URL"))
 
 @app.route("/")
 def index():
